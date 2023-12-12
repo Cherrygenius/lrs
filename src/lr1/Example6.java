9d19a5b5b1a047a1f44bce48c72bfbcd46845922
Maksim <lion_ska@list.ru>
2023-12-12 15:38:51 +0500

    gl

---
+++ src/lr1/Example6.java
@@ -0,0 +1,18 @@
+package lr1;
+
+import java.util.Scanner;
+
+public class Example6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите фамилию:");
+        String lastName = scanner.nextLine();
+        System.out.println("Введите имя:");
+        String firstName = scanner.nextLine();
+        System.out.println("Введите отчество:");
+        String surname = scanner.nextLine();
+        scanner.close();
+        System.out.println("Привет" + "," + " " + lastName + " " + firstName + " " + surname);
+
+    }
+}
