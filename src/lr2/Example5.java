3b5fc1225b36ffca70cc6d0a9631f6896c08d721
Maksim <lion_ska@list.ru>
2023-12-13 12:59:46 +0500

    gl

---
+++ src/lr2/Example5.java
@@ -0,0 +1,12 @@
+package lr2;
+
+import java.util.Scanner;
+
+public class Example5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите число: ");
+        int number = scanner.nextInt();
+        System.out.println(number/1000);
+    }
+}
